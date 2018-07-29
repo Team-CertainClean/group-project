f3e902a110dff95fbcbf1065fcfffacdf6cfeb78
Nate Gelle <gelle.nate@gmail.com>
2018-07-29 15:20:37 -0500

    create contact info component

---
+++ src/components/AvailabilitySelectView/contactInfo.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {compose} from 'redux';
+import { USER_ACTIONS } from '../../redux/actions/userActions';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  card: {
+    minWidth: 275,
+  },
+  title: {
+    marginBottom: 16,
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
+
+const mapStateToProps = state => ({
+    user: state.user,
+});
+
+class ContactInfo extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    componentDidMount() {
+        this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div>
+                <Card className={classes.card}>
+                    <CardContent>
+                        <Typography className={classes.title} color="textPrimary">
+                            certainclean
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button variant="contained" size="small">Contact Us</Button>
+                    </CardActions>
+                </Card>
+            </div>
+        );
+    }
+}
+
+ContactInfo.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+  
+
+export default compose(withStyles(styles),connect(mapStateToProps))(ContactInfo);
