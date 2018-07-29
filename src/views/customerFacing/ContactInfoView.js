47c8263bee016e58b144c6f74a146e5d421d16d7
jamesmeyers88 <jamesmeyers88@gmail.com>
2018-07-29 02:38:44 -0500

    create basic form

--- src/views/customerFacing/ContactInfoView.js
+++ src/views/customerFacing/ContactInfoView.js
@@ -5,6 +5,7 @@ import Nav from '../../components/Nav/Nav';
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const mapStateToProps = state => ({
   user: state.user,
@@ -31,15 +32,36 @@ class ContactInfoView extends Component {
                     <TextField
                         id="name"
                         label="Name"
-                        
                         value={this.state.name}
                         onChange={this.handleChange('name')}
                         margin="normal"
                     />
                     <TextField
-                        id="textarea"
-                        label="With placeholder multiline"
-                        placeholder="Placeholder"
+                        id="email"
+                        label="Email"
+                        value={this.state.email}
+                        onChange={this.handleChange('email')}
+                        margin="normal"
+                    />
+                    <TextField
+                        id="address"
+                        label="Address"
+                        value={this.state.address}
+                        onChange={this.handleChange('address')}
+                        margin="normal"
+                    />
+                    <TextField
+                        id="cityStateZip"
+                        label="City, State, Zip"
+                        value={this.state.cityStateZip}
+                        onChange={this.handleChange('cityStateZip')}
+                        margin="normal"
+                    />
+                    <TextField
+                        id="phone"
+                        label="Phone"
+                        value={this.state.phone}
+                        onChange={this.handleChange('phone')}
                         margin="normal"
                     />
             </form>
