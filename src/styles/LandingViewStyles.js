516bbfbd6566a28096e04a789ef10c9d9da13ac3
Flannel Bear <josh.flannelbear@gmail.com>
2018-07-30 10:27:06 -0500

    implement variable routes for get started button depending on the selected location type

--- src/styles/LandingViewStyles.js
+++ src/styles/LandingViewStyles.js
@@ -7,6 +7,9 @@ const styles = {
     },
     unselectedLocationType: {
         backgroundColo: 'grey'
+    },
+    getStartedButton: {
+        textDecorationLine: 'none'
     }
 } 
 
--- src/views/customerFacing/LandingView.js
+++ src/views/customerFacing/LandingView.js
@@ -3,55 +3,87 @@ import React from 'react';
 // Material UI Imports
 import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import {withStyles} from '@material-ui/core/styles';
 import styles from '../../styles/LandingViewStyles';
+import { Typography } from '../../../node_modules/@material-ui/core';
 
 // Component Imports
+import { Link } from 'react-router-dom';
 
 class CustomerLandingView extends React.Component{
     constructor(){
         super();
-        this.state = {selection: null}
+        this.state = {selection: null, path: '/addroom'}
     }
 
-    selectLocationType = (event) => {
+    selectLocationType = (type) => {
         if(this.state.selection === null){
-            if(event.target.value === "Residential"){
-                this.setState({selection: false});
+            if(type === "residential"){
+                this.setState({selection: true, path: '/addroom'});
             } else {
-                this.setState({selection: true});
+                this.setState({selection: false, path: '/contact'});
             }
         } else {
-            this.setState({selection: !this.state.selection});
+            if(type === "residential"){
+                this.setState({selection: true, path: '/addroom'});
+            } else {
+                this.setState({selection: false, path: '/contact'});
+            }
         }
     }
 
     render(){
 
         const { classes } = this.props;
+
         let locationTypeChoices = null;
+        let locationTypeTitle = null;
+        let locationTypeContent = null;
         if(this.state.selection === null){
+            locationTypeTitle = (
+                <Typography variant="title">Choose a location type</Typography>
+            );
+            locationTypeContent = (
+                <Typography>
+                    Choose Residential for cleaning a home, or an airbnb.
+                </Typography>
+            );
             locationTypeChoices = (
                 <div>
-                    <Button onClick={this.selectLocationType} className={classes.unselectedLocationType}>Residential</Button>
-                    <Button onClick={this.selectLocationType} className={classes.unselectedLocationType}>Commercial</Button>
+                    <Button onClick={() => this.selectLocationType('residential')} className={classes.unselectedLocationType} value="residential">Residential</Button>
+                    <Button onClick={() => this.selectLocationType('commercial')} className={classes.unselectedLocationType} value="commercial">Commercial</Button>
                 </div>
             );
         } else if(this.state.selection){
+            locationTypeTitle = (
+                <Typography variant="title">Residential</Typography>
+            );
+            locationTypeContent = (
+                <Typography>
+                    What to expect: You'll fill out our estimator to receive an estimated duration your cleaning will take and then we'll contact you when we've confirmed.
+                </Typography>
+            );
             locationTypeChoices = (
                 <div>
-                    <Button onClick={this.selectLocationType} className={classes.selectedLocationType}>Residential</Button>
-                    <Button onClick={this.selectLocationType} className={classes.unselectedLocationType}>Commercial</Button>
+                    <Button onClick={() => this.selectLocationType('residential')} className={classes.selectedLocationType}>Residential</Button>
+                    <Button onClick={() => this.selectLocationType('commercial')} className={classes.unselectedLocationType}>Commercial</Button>
                 </div>
             );
         } else {
+            locationTypeTitle = (
+                <Typography variant="title">Commercial</Typography>
+            );
+            locationTypeContent = (
+                <Typography>
+                    What to expect: You'll be navigated to our contact form, and then we will get in touch to discuss the cleaning in further detail.
+                </Typography>
+            );
             locationTypeChoices = (
                 <div>
-                    <Button onClick={this.selectLocationType} className={classes.unselectedLocationType}>Residential</Button>
-                    <Button onClick={this.selectLocationType} className={classes.selectedLocationType}>Commercial</Button>
+                    <Button onClick={() => this.selectLocationType('residential')} className={classes.unselectedLocationType}>Residential</Button>
+                    <Button onClick={() => this.selectLocationType('commercial')} className={classes.selectedLocationType}>Commercial</Button>
                 </div>
             );
         }
@@ -60,13 +92,12 @@ class CustomerLandingView extends React.Component{
             <Paper>
                 {locationTypeChoices}
                 <Card>
-                    <CardHeader>
-
-                    </CardHeader>
                     <CardContent>
-
+                        {locationTypeTitle}
+                        {locationTypeContent}
                     </CardContent>
                 </Card>
+                <Link to={this.state.path} className={classes.getStartedButton}><Button>Get Started</Button></Link>
             </Paper>
         );
     }
