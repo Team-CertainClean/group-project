0986850b92ec5df9cc855d3792c233cb7b3f57fd
jamesmeyers88 <jamesmeyers88@gmail.com>
2018-07-30 14:46:53 -0500

    added cleaning_type to localState commented out genQuery.js

--- server/modules/router-modules/request-router/genQuery.js
+++ server/modules/router-modules/request-router/genQuery.js
@@ -1,118 +1,118 @@
-function genQuery(reqBody){
-    let query = {text: '', array: []};
-    let body = reqBody;
-    body = sanitizeBody(body);
-    query = processBody(body, query);
-    return query;
-}
+// function genQuery(reqBody){
+//     let query = {text: '', array: []};
+//     let body = reqBody;
+//     body = sanitizeBody(body);
+//     query = processBody(body, query);
+//     return query;
+// }
 
-function processBody(body, query){
+// function processBody(body, query){
 
-}
+// }
 
-let recursion = 0;
+// let recursion = 0;
 
-// Recursive function for sanitizing user inputs in request body
-function sanitizeBody(body){
+// // Recursive function for sanitizing user inputs in request body
+// function sanitizeBody(body){
     
-    recursion += 1;
+//     recursion += 1;
 
-    let sanBody = body;
+//     let sanBody = body;
 
-    console.log(sanBody);
+//     console.log(sanBody);
 
-    for(let object in sanBody){
+//     for(let object in sanBody){
 
-        console.log(sanBody[object]);
+//         console.log(sanBody[object]);
 
-        if(typeof(sanBody[object]) === 'string'){
+//         if(typeof(sanBody[object]) === 'string'){
 
-            let sanString = sanBody[object];
+//             let sanString = sanBody[object];
 
-            sanString = sanitizeString(sanBody[object]);
+//             sanString = sanitizeString(sanBody[object]);
 
-            sanBody[object] = sanString;
+//             sanBody[object] = sanString;
 
-        }else if(Array.isArray(sanBody[object])) {
+//         }else if(Array.isArray(sanBody[object])) {
 
-            console.log("In array type");
+//             console.log("In array type");
 
-            for(let i = 0; i < sanBody[object].length; i++){
+//             for(let i = 0; i < sanBody[object].length; i++){
 
-                console.log("Index of array: ", sanBody[object][i]);
+//                 console.log("Index of array: ", sanBody[object][i]);
 
-                if(typeof(object[i]) === 'object'){
+//                 if(typeof(object[i]) === 'object'){
 
-                    console.log("nested object: ", sanBody[object][i]);
+//                     console.log("nested object: ", sanBody[object][i]);
 
-                    sanitizeBody(sanBody[object][i]);
+//                     sanitizeBody(sanBody[object][i]);
 
-                } else if(Array.isArray(sanBody[object][i])){
+//                 } else if(Array.isArray(sanBody[object][i])){
 
-                    console.log("nested array: ", sanBody[object][i]);
+//                     console.log("nested array: ", sanBody[object][i]);
 
-                    sanitizeBody(sanBody[object][i]);
+//                     sanitizeBody(sanBody[object][i]);
 
-                } else {
+//                 } else {
 
-                    if(typeof(object[i]) === 'string'){
+//                     if(typeof(object[i]) === 'string'){
 
-                        console.log("nested string: ", sanBody[object][i]);
+//                         console.log("nested string: ", sanBody[object][i]);
 
-                        sanBody[object][i] = sanitizeString(sanBody[object[i]]);
+//                         sanBody[object][i] = sanitizeString(sanBody[object[i]]);
 
-                        console.log(object[i]);
+//                         console.log(object[i]);
 
-                    }
-                }
-            }
-        } else if(typeof(sanBody[object]) == 'object'){
-            console.log("In object type");
-            sanitizeBody(sanBody[object]);
-            // for(let prop in object){
-            //     console.log("object type property: ", object[prop]);
-            //     if(typeof(object[prop]) == 'object'){
-            //         console.log("object: ", object[prop]);
-            //         sanitizeBody(object[prop]);
-            //     } else {
-            //         if(typeof(object[prop]) == 'string'){
-            //             console.log("string: ", object[prop]);
-            //             object[prop] = sanitizeString(object[prop]);
-            //             console.log("sanString: ",  object[prop]);
-            //         }
-            //     }
-            // }
-        }
-    }
-    return sanBody;
-}
+//                     }
+//                 }
+//             }
+//         } else if(typeof(sanBody[object]) == 'object'){
+//             console.log("In object type");
+//             sanitizeBody(sanBody[object]);
+//             // for(let prop in object){
+//             //     console.log("object type property: ", object[prop]);
+//             //     if(typeof(object[prop]) == 'object'){
+//             //         console.log("object: ", object[prop]);
+//             //         sanitizeBody(object[prop]);
+//             //     } else {
+//             //         if(typeof(object[prop]) == 'string'){
+//             //             console.log("string: ", object[prop]);
+//             //             object[prop] = sanitizeString(object[prop]);
+//             //             console.log("sanString: ",  object[prop]);
+//             //         }
+//             //     }
+//             // }
+//         }
+//     }
+//     return sanBody;
+// }
 
-function sanitizeString(string){
-    let sanString = string;
-    for(let i = 0; i < sanString.length; i++){
-        if(sanString[i] === '"'){
-            console.log("doublequote found!: ", sanString[i]);
-            sanString.replace(/\"/g,'\\"');
-            console.log(sanString);
-        } else if(sanString[i] === "'"){
-            console.log("singlequote found!: ", sanString[i]);
-            console.log(sanString.replace(/\'/g,"\\'"));
-            console.log(sanString);
-        }
-    }
-    return sanString;
-}
+// function sanitizeString(string){
+//     let sanString = string;
+//     for(let i = 0; i < sanString.length; i++){
+//         if(sanString[i] === '"'){
+//             console.log("doublequote found!: ", sanString[i]);
+//             sanString.replace(/\"/g,'\\"');
+//             console.log(sanString);
+//         } else if(sanString[i] === "'"){
+//             console.log("singlequote found!: ", sanString[i]);
+//             console.log(sanString.replace(/\'/g,"\\'"));
+//             console.log(sanString);
+//         }
+//     }
+//     return sanString;
+// }
 
 
-const testObject = {
-    num: 0,
-    string1: "'Hello",
-    string2: 'Goodbye"',
-    array: ['sup"', {string3: 'nestedObject"'}, 0],
-    object: {nestedObject: { num: 10 }, string4: '"string'}
-}
+// const testObject = {
+//     num: 0,
+//     string1: "'Hello",
+//     string2: 'Goodbye"',
+//     array: ['sup"', {string3: 'nestedObject"'}, 0],
+//     object: {nestedObject: { num: 10 }, string4: '"string'}
+// }
 
-console.log(sanitizeBody(testObject));
-console.log("iterations: ", recursion);
+// console.log(sanitizeBody(testObject));
+// console.log("iterations: ", recursion);
 
-module.exports = genQuery;
+// module.exports = genQuery;
