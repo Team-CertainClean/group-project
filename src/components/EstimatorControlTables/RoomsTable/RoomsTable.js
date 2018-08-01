0c584ba63c8dae9d43c6f14e80773465a47c94d0
Flannel Bear <josh.flannelbear@gmail.com>
2018-08-01 16:11:36 -0500

    edit server side get for /api/room to include location_type

--- src/components/EstimatorControlTables/RoomsTable/RoomsTable.js
+++ src/components/EstimatorControlTables/RoomsTable/RoomsTable.js
@@ -5,6 +5,12 @@ import { connect } from 'react-redux';
 // Material UI Imports
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import Button from '@material-ui/core/Button';
 
 // Component Imports 
 import AddRoomForm from './AddRoomForm';
@@ -47,10 +53,40 @@ class RoomControlTable extends React.Component{
     }
 
     render(){
+        let table = null;
+        if(this.props.rooms){
+            table = (
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Room Name</TableCell>
+                            <TableCell>Location Type</TableCell>
+                            <TableCell>Duration Metric</TableCell>
+                            <TableCell>Edit</TableCell>
+                            <TableCell>Hide</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.props.rooms.map(room => {
+                            return(
+                                <TableRow key={room.id}>
+                                    <TableCell>{room.room_name}</TableCell>
+                                    <TableCell>{room.location_type}</TableCell>
+                                    <TableCell>{room.metric}</TableCell>
+                                    <TableCell><Button>Edit</Button></TableCell>
+                                    <TableCell>Insert check box</TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            );
+        }
         return(
             <Paper>
                 <Typography variant="title">Add Rooms</Typography>
                 <AddRoomForm handleChangeFor={this.handleChangeFor} submitRoom={this.submitRoom} room={this.state.roomInfo.room_name} metric={this.state.roomInfo.metric} anchor={this.state.anchor} locations={this.props.locations}/>
+                {table}
             </Paper>
         );
     }
