c2abce83e8a5072c1c2a29310fa44594dba90050
Nate Gelle <gelle.nate@gmail.com>
2018-08-12 14:24:38 -0500

    allow admin to customer calendar comm

--- server/modules/router-modules/availability-router/invertCalendar.js
+++ server/modules/router-modules/availability-router/invertCalendar.js
@@ -4,6 +4,7 @@
     the admin has placed a calendar object.
 */
 async function invertCalendar(calendarArray){
+    console.log("Invert Calendar: ", calendarArray);
     return new Promise( async (resolve) => {
         let unavailableArray = [];
         for(let i = 0; i < calendarArray.length; i++){
@@ -11,7 +12,9 @@ async function invertCalendar(calendarArray){
             let previousIndex = currentIndex - 1;
             if(currentIndex === 0){
                 unavailableArray[i] = await fillInCalendar(calendarArray[currentIndex], "undefined", currentIndex, calendarArray.length - 1);
+                console.log("After fillin await, unavail: ", unavailableArray);
             } else {
+                console.log('calendarArray in invertCalendar', calendarArray);
                 result = await fillInCalendar(calendarArray[currentIndex], calendarArray[previousIndex], currentIndex, calendarArray.length - 1);
                 if(Array.isArray(result)){
                     for(let object of result){
@@ -20,22 +23,30 @@ async function invertCalendar(calendarArray){
                 } else {
                     unavailableArray[unavailableArray.length] = result;
                 }
+                console.log("After fillin await, unavail: ", unavailableArray);
             }
         }
+        console.log("Completed Invert: ", unavailableArray);
+        unavailableArray.shift();
         resolve(unavailableArray);
     });
 }
 
 function fillInCalendar(currentDate, prevDate, currentIndex, maxIndex){
+    console.log("Fillin Calendar: ", currentDate, prevDate);
     return new Promise((resolve, reject) => {
         if(prevDate === "undefined"){
-            if(currentDate.start.getHours() > 5){
+            console.log("Prev is undefined");
+            console.log("currentDate start hours: ", currentDate.start.getHours());
+            if(currentDate.start.getHours() >= 5){
+                console.log("Got hours, minutes are: ", currentDate.start.getMinutes());
                 if(currentDate.start.getMinutes() > 0){
                     let result = {};
                     result.end = new Date(currentDate.start);
                     result.start = new Date(currentDate.start);
                     result.start.setHours(5);
                     result.start.setMinutes(0);
+                    console.log("First run: ", result);
                     resolve(result);
                 } else {
                     resolve();
@@ -53,14 +64,14 @@ function fillInCalendar(currentDate, prevDate, currentIndex, maxIndex){
                resolve(result);
         } 
         else if(currentIndex == maxIndex){
-            if(currentDate.end.getHours() < 19){
+            if(currentDate.end.getHours() < 23){
                 let result = {};
                 let endCalendar = {};
                 result.start = new Date(prevDate.end);
                 result.end = new Date(currentDate.start);
                 endCalendar.start = new Date(currentDate.end);
                 endCalendar.end = new Date(currentDate.end);
-                endCalendar.end.setHours(19);
+                endCalendar.end.setHours(23);
                 endCalendar.end.setMinutes(0);
                 resolve([result, endCalendar]);
             } else {
